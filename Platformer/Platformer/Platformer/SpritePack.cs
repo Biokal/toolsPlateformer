0d5552a9a12aed49568c86e1425181612c311659
KobaltEntity <Tanguy.Couturier@outlook.com>
2018-12-11 15:27:04 +0100

    Walls

---
+++ Platformer/Platformer/Platformer/SpritePack.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    class SpritePack
+    {
+        GraphicsDevice m_RegisteredDevice;
+        ContentManager m_ContentManager;
+        static SpritePack m_Instance;
+        Dictionary<string, Texture2D> m_Textures;
+
+
+        public SpritePack(GraphicsDevice _graphics, ContentManager _contents)
+        {
+            m_Instance = this;
+            m_Textures = new Dictionary<string, Texture2D>();
+            m_RegisteredDevice = _graphics;
+            m_ContentManager = _contents;
+        }
+
+        static public SpritePack GetSingleton()
+        {
+            return m_Instance;
+        }
+
+        public Texture2D GetTexture(string Name)
+        {
+            if(m_Textures[Name] == null)
+            {
+                Texture2D newTexture = m_ContentManager.Load<Texture2D>(@"Sprite/"+ Name+".png");
+                m_Textures.Add(Name, newTexture);
+            }
+
+            return m_Textures[Name];
+        }
+
+    }
+}
