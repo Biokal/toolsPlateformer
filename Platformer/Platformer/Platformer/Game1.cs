453dbb2e8ca396857a8cbf488b0bb8839d21cd45
KobaltEntity <Tanguy.Couturier@outlook.com>
2018-12-11 17:01:30 +0100

    Merge

--- Platformer/Platformer/Platformer/Game1.cs
+++ Platformer/Platformer/Platformer/Game1.cs
@@ -17,11 +17,7 @@ namespace Platformer
         SpritePack spritePack;
 
         Player player;
-        Wall wall1;
-
         Camera playerCamera;
-        //Liste des murs et des sols pour les collisions
-        List<Wall> wallList = new List<Wall>();
 
 
         public Game1()
@@ -40,14 +36,12 @@ namespace Platformer
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            wallPool = new WallPool();
 
             player = new Player(GraphicsDevice, new Vector2(32, 32), 32, 32, Color.Red);
             playerCamera = new Camera(GraphicsDevice.Viewport.Width,GraphicsDevice.Viewport.Height);
 
             //Création de murs temporaires ofc
-            wall1 = new Wall(new Vector2(0, 128));
-            wallList.Add(wall1);
             // TODO: use this.Content to load your game content here
         }
 
@@ -63,7 +57,7 @@ namespace Platformer
 
             // TODO: Add your update logic here
 
-            player.Update(gameTime,wallList);
+            //player.Update(gameTime,wallList);
             playerCamera.setCameraPosition(player.getPosition() - player.getOrigin() + new Vector2(player.getRotation()/90*32,0));
 
 
@@ -77,7 +71,7 @@ namespace Platformer
             // TODO: Add your drawing code here
             spriteBatch.Begin();
             player.Draw(spriteBatch,playerCamera);
-            wall1.Draw(spriteBatch, playerCamera);
+            //wall1.Draw(spriteBatch, playerCamera);
             spriteBatch.End();
 
 
---
+++ Platformer/Platformer/Platformer/Map.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    class Map
+    {
+        List<Wall> m_MapWalls;
+        WallPool m_WallPool;
+
+        public Map()
+        {
+            m_MapWalls = new List<Wall>();
+            m_WallPool = new WallPool();
+        }
+
+        public LoadMap()
+        {
+
+        }
+
+    }
+}
